9f2aaeb2a1b0ef771b9ebd1138f58ce41ff974f0
Dmitry <neox56@gmail.com>
2022-05-02 19:59:01 +0400

    Add routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './app.module.scss';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from '../header';
+import Auth from '../auth';
+import Register from '../register';
+import {AboutPage, FavoritePage, MainPage} from '../pages';
+import CharactersList from '../characters-list';
+import LocationsList from '../locations-list';
+import EpisodesList from '../episodes-list';
+
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
+  return <main role="main" className="container">
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/main" element={<MainPage/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/favorites" element={<FavoritePage/>}/>
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/characters" element={<CharactersList/>}/>
+        <Route path="/locations" element={<LocationsList/>}/>
+        <Route path="/episodes" element={<EpisodesList/>}/>
+      </Routes>
+    </BrowserRouter>
+  </main>;
 };
 
 export default App;
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,5 @@
-import SignInPage from './sign-in-page';
-import SignUpPage from './sign-up-page';
 import MainPage from './main-page';
 import FavoritePage from './favorite-page';
 import AboutPage from './about-page';
 
-export {SignInPage, SignUpPage, MainPage, FavoritePage, AboutPage};
+export {MainPage, FavoritePage, AboutPage};
