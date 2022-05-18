ddd75be365edf3d86a09818c2c3bdeda2de0cb52
Dmitry <neox56@gmail.com>
2022-05-18 14:36:18 +0400

    Add about-page

--- src/components/pages/about-page.js
+++ src/components/pages/about-page.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './about-page.scss';
+import {NavLink} from 'react-router-dom';
+import Button from '../UI/button';
+import backBtn from '../../assets/icons/black-arrow.svg';
 
 const AboutPage = () => {
   return (
-    <div>
-      AboutPage
-    </div>
+    <main className="favorite-main g-4 mt-5">
+      <div className="row row-cols-1 col-md-12 text-center mb-5 align-items-center">
+        <NavLink to="/" className="col-md-4 col-sm-12">
+          <Button type={'back-btn'}>
+            <img src={backBtn} alt="backBtn"/>
+            <span className="back-btn-text mx-2">Назад</span>
+          </Button>
+        </NavLink>
+        <div className="about-title col-md-4 col-sm-12">О проекте</div>
+        <div className="about-text d-flex flex-column justify-content-center align-items-center px-2 mt-5 ">
+          <p> Данный проект создан по мотивам приключений Рика и Морти.
+            Здесь вы можете поближе познакомиться со всеми персонажами, эпизодами и локациями данного мультсериала.
+          </p>
+          <p> Проект является тестовым заданием для входящих кандидатов пула Frontend.
+            В зависимости от грейда кандидата необходимо выполнить соответствующий список заданий. Желаем удачи!
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
